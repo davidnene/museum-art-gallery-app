26c2719c359461fa269087e2e7770b8f80a21511
davidnene <davienesh4@gmail.com>
2022-09-12 22:16:33 +0300

    Add useHistory hook

--- Frontend-react/artgalleryapp/src/DisplayRatings.js
+++ Frontend-react/artgalleryapp/src/DisplayRatings.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 
 
 function DisplayRatings({ arts }) {
     const params = useParams()
+    const history = useHistory()
 
     const id = params.id - 418
+
+    function handleClickBack() {
+        history.push('/')
+    }
 
     return (
         <div className="card gx-1" style={{width: 30 + "rem", position:"absolute", right:"450px"}}>
@@ -13,9 +18,9 @@ function DisplayRatings({ arts }) {
         <div className="card-body">
             <h5 className="card-title">{arts[id].title}</h5>
             <p className="card-text">Artist: {arts[id].artist_title}</p>
-            {/* <button style={{position:"absolute", right:"30px"}} onClick={handleClickBack} className="btn btn-danger">Back</button>     */}
+            <button style={{position:"absolute", right:"30px"}} onClick={handleClickBack} className="btn btn-danger">Back</button>
                 <div>
-                    <table class="table">
+                    <table className="table">
                         <thead>
                             <tr>
                             <th scope="col">Rating ID</th>
@@ -25,11 +30,11 @@ function DisplayRatings({ arts }) {
                         </thead>
                         <tbody>
                         {arts[id].ratings.map(rating => (
-                            <tr>
-                            <th scope="row">{rating.id}</th>
+                            <tr key = {rating.id}>
+                            <th  scope="row">{rating.id}</th>
                             <td>{rating.art_rating}/10</td>
                             <td>{rating.comment}</td>
-                            <td><button>X</button></td>
+                            <td><button>Remove</button></td>
                             </tr>
                             ))}
                         </tbody>
