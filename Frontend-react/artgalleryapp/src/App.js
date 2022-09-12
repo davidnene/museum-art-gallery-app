0a0024f78ea8b850122c98ce8ce6201df96a1926
davidnene <davienesh4@gmail.com>
2022-09-12 21:01:36 +0300

    Configure routes

--- Frontend-react/artgalleryapp/src/App.js
+++ Frontend-react/artgalleryapp/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import {useState, useEffect} from 'react';
 import Card from './Card';
+import DisplayRatings from './DisplayRatings';
+import {Route, Switch} from 'react-router-dom'
 
 function App() {
   const [arts, setArts] = useState([])
@@ -14,11 +16,20 @@ function App() {
   useEffect(handleArts, [])
 
   return (
+    <div>
+  <Switch>
+  <Route exact path={`/:id`}>
+        <DisplayRatings arts = {arts}/>
+  </Route>
+  <Route exact path={'/'}>
   <div className='container'>
     <div className='row'>
       {arts.map(art => (
-        <Card key = {art.id} title = {art.title} image = {art.img_url} altText = {art.alt_text} artistTitle = {art.artist_title} dateStart = {art.date_start} dateEnd = {art.date_end} ></Card> ))}
+        <Card key = {art.id} id = {art.id} title = {art.title} image = {art.img_url} altText = {art.alt_text} artistTitle = {art.artist_title} dateStart = {art.date_start} dateEnd = {art.date_end} ></Card> ))}
     </div>
+  </div>
+  </Route>
+  </Switch>  
   </div>
   )
 };
