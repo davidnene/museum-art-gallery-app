bada158d378940877ab3a70200f405b47443b760
davidnene <davienesh4@gmail.com>
2022-09-13 16:22:56 +0300

    Polish layout

--- Frontend-react/artgalleryapp/src/App.js
+++ Frontend-react/artgalleryapp/src/App.js
@@ -25,9 +25,9 @@ function App() {
         <DisplayRatings arts = {arts}/>
   </Route>
   <Route exact path={'/'}>
-    <Header/>
-  <div className='container'>
-    <div className='row'>
+    <Header/><br/>
+  <div className='container img-center'>
+    <div className='row row-cols-auto'>
       {arts.map(art => (
         <Card key = {art.id} id = {art.id} title = {art.title} image = {art.img_url} altText = {art.alt_text} artistTitle = {art.artist_title} dateStart = {art.date_start} dateEnd = {art.date_end} setArts = {setArts} arts = {arts} setRefresh = {setRefresh} refresh = {refresh}></Card> ))}
     </div>
